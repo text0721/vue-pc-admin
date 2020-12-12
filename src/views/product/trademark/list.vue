6131cb77d9cafd4b3c0606aac7639c5f763a26d9
shuailing <1986087793@qq.com>
2020-12-12 08:33:41 +0800

    feat:完成添加基础样式

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <!-- 点击添加展示的对话框 -->
+    <el-dialog title="添加品牌" :visible.sync="formVisible">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="品牌名称" prop="name">
+          <el-input v-model="ruleForm.tmName"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌logo" prop="region">
+          <!-- <el-input v-model="ruleForm.logoUrl"></el-input> -->
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="formVisible = false">取消</el-button>
+          <el-button type="primary" @click="addTradeMark">确定</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+    <el-button type="primary" icon="el-icon-plus" @click="formVisible = true"
+      >添加</el-button
+    >
     <el-table :data="trademark" border style="width: 100%; margin-top: 20px">
       <el-table-column type="index" label="序号" width="80" align="center">
       </el-table-column>
@@ -58,6 +89,49 @@ export default {
       current: 1, // 代表当前页码
       size: 3, // 代表每页显示的条数
       total: 0,
+      formVisible: false, //定义对话框默认不显示
+      ruleForm: {
+        tmName: "",
+        logoUrl: "",
+      },
+      imageUrl: "",
+      rules: {
+        name: [
+          { required: true, message: "请输入活动名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        region: [
+          { required: true, message: "请选择活动区域", trigger: "change" },
+        ],
+        date1: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        date2: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择时间",
+            trigger: "change",
+          },
+        ],
+        type: [
+          {
+            type: "array",
+            required: true,
+            message: "请至少选择一个活动性质",
+            trigger: "change",
+          },
+        ],
+        resource: [
+          { required: true, message: "请选择活动资源", trigger: "change" },
+        ],
+        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }],
+      },
     };
   },
   methods: {
@@ -86,6 +160,10 @@ export default {
       this.current = 1;
       this.getPagesTradeMarkList(this.current, size);
     },
+    //定义添加品牌的方法
+    addTradeMark() {
+      this.formVisible = false;
+    },
   },
   mounted() {
     this.getPagesTradeMarkList(this.current, this.size);
@@ -104,4 +182,27 @@ export default {
 
 /deep/.el-pagination__jump
   margin-left: 250px
+
+>>>.avatar-uploader .el-upload
+  border: 1px dashed #d9d9d9
+  border-radius: 6px
+  cursor: pointer
+  position: relative
+  overflow: hidden
+
+  >>>.avatar-uploader .el-upload:hover
+    border-color: #409EFF
+
+  >>>.avatar-uploader-icon
+    font-size: 28px
+    color: #8c939d
+    width: 178px
+    height: 178px
+    line-height: 178px
+    text-align: center
+
+  >>>.avatar
+    width: 178px
+    height: 178px
+    display: block
 </style>
